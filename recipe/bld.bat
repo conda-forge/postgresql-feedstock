d1429da9931609d745f754c89aaff0bce40f728d
Michael Sarahan <msarahan@gmail.com>
2016-07-27 16:47:45 -0500

    try removing appveyor's postgres install

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,6 +4,11 @@ echo $config-^>{openssl} = '%LIBRARY_PREFIX%'; >> config.pl
 echo $config-^>{zlib} = '%LIBRARY_PREFIX%';    >> config.pl
 echo $config-^>{python} = '%PREFIX%';          >> config.pl
 
+:: Appveyor's postgres install is on PATH and interferes with testing
+IF "%APPVEYOR%" == "TRUE" (
+    RD /S /Q "C:\Program Files\PostgreSQL"
+)
+
 :: Need to move a more current msbuild into PATH.  32-bit one in particular on AppVeyor barfs on the solution that
 ::    Postgres writes here.  This one comes from the Win7 SDK (.net 4.0), and is known to work.
 if "%ARCH%" == "32" (
