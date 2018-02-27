5f7d70bbbce9cabca07c11ff084bb6690541e821
Patrick Sodré <sodre@sodre.co>
2018-02-27 00:01:15 -0500

    Replace LD_LIBRARY_PATH hack with -rpath build option

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-if [ "$(uname)" == "Darwin" ]; then
-    LDFLAGS="-rpath $PREFIX/lib $LDFLAGS"
-fi
+LDFLAGS="-rpath $PREFIX/lib $LDFLAGS"
 
 ./configure \
     --prefix=$PREFIX \
@@ -20,11 +18,8 @@ make -j $CPU_COUNT
 make -j $CPU_COUNT -C contrib
 
 make check
-# plpython can't find libpython3 during check time on Linux only.
-# We add the LD_LIBRARY_PATH to assist on that issue. This does not
-# impact the library after it is installed.
-LD_LIBRARY_PATH=$PREFIX/lib make check -C src/pl
-LD_LIBRARY_PATH=$PREFIX/lib make check -C contrib
+make check -C src/pl
+make check -C contrib
 make check -C src/interfaces/ecpg
 
 make install
