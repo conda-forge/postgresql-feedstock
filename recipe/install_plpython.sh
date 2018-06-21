64543a88d5d922b5a6359e10c817566b7d4798bd
Marius van Niekerk <marius.v.niekerk@gmail.com>
2018-06-21 18:16:09 -0400

    ensure contrib is built too

--- recipe/install_plpython.sh
+++ recipe/install_plpython.sh
@@ -1,20 +1,22 @@
 set -x
 set -e
-
-cd src/pl/plpython
 export PYTHON=$PREFIX/bin/python
 
-./configure \
-    --prefix=$PREFIX \
-    --with-readline \
-    --with-libraries=$PREFIX/lib \
-    --with-includes=$PREFIX/include \
-    --with-openssl \
-    --with-uuid=e2fs \
-    --with-libxml \
-    --with-libxslt \
-    --with-gssapi
-    --with-python
+for dir in src/pl/plpython contrib/hstore_plpython; do
+  pushd $dir
+  ./configure \
+      --prefix=$PREFIX \
+      --with-readline \
+      --with-libraries=$PREFIX/lib \
+      --with-includes=$PREFIX/include \
+      --with-openssl \
+      --with-uuid=e2fs \
+      --with-libxml \
+      --with-libxslt \
+      --with-gssapi
+      --with-python
 
-make
-make install
+  make
+  make install
+  popd
+done
