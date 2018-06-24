16ea50e8a6d64de60ef402546771255ddce6a5e6
Marius van Niekerk <marius.v.niekerk@gmail.com>
2018-06-23 20:50:19 -0400

    move configure

--- recipe/install_plpython.sh
+++ recipe/install_plpython.sh
@@ -1,21 +1,20 @@
 set -x
 set -e
 export PYTHON=$PREFIX/bin/python
+./configure \
+    --prefix=$PREFIX \
+    --with-readline \
+    --with-libraries=$PREFIX/lib \
+    --with-includes=$PREFIX/include \
+    --with-openssl \
+    --with-uuid=e2fs \
+    --with-libxml \
+    --with-libxslt \
+    --with-gssapi
+    --with-python
 
 for dir in src/pl/plpython contrib/hstore_plpython; do
   pushd $dir
-  ./configure \
-      --prefix=$PREFIX \
-      --with-readline \
-      --with-libraries=$PREFIX/lib \
-      --with-includes=$PREFIX/include \
-      --with-openssl \
-      --with-uuid=e2fs \
-      --with-libxml \
-      --with-libxslt \
-      --with-gssapi
-      --with-python
-
   make
   make install
   popd
