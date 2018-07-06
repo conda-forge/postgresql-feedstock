855752408e16ec4d683d812768d7f7440923a692
Marius van Niekerk <marius.v.niekerk@gmail.com>
2018-07-06 18:13:08 -0400

    Add PERL5LIB=.;%PERL5LIB% to windows build files

--- recipe/install_db.bat
+++ recipe/install_db.bat
@@ -1,2 +1,6 @@
 pushd src\tools\msvc
+
+REM because modern versions of perl don't have this by default 
+set PERL5LIB=.;%PERL5LIB%
+
 call install.bat %LIBRARY_PREFIX%
--- recipe/install_runtime.bat
+++ recipe/install_runtime.bat
@@ -1,4 +1,8 @@
 pushd src\tools\msvc
+
+REM because modern versions of perl don't have this by default 
+set PERL5LIB=.;%PERL5LIB%
+
 call install.bat %LIBRARY_PREFIX%
 mkdir backup
 MOVE %LIBRARY_BIN%\libpq.dll backup
